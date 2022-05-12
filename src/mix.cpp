a76093404366bab51af06197849294a339ead8fd
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2022-05-12 18:28:16 -0300

    feature: mix program working implementation, only rgb for now

--- src/mix.cpp
+++ src/mix.cpp
@@ -46,6 +46,7 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 	settings.output_extension = "png";
 
 	int paths_amount = 0;
+	int non_skipped_paths_amount = 0;
 	for (auto arg_ptr = args.begin(); arg_ptr != args.end(); ++arg_ptr) {
 		std::string current = *arg_ptr;
 
@@ -75,9 +76,16 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 
 		bool arg_starts_with_underscore = current[0] == '_';
 		if (arg_starts_with_underscore) {
-			cout << "[mix] channel taken from path \"" << current << "\" will be filled with zeros (arg starts with _)\n";
+			const char* current_channel =
+				paths_amount == 1 ? "red"   :
+				paths_amount == 2 ? "green" :
+				paths_amount == 3 ? "blue"  : "alpha";
+
+			cout << "[mix] " << current_channel << " channel will be skipped (filled with zeros)\n";
 			continue;
 		}
+
+		non_skipped_paths_amount++;
 
 		if (paths_amount == 1) {
 			settings.r_input_path = current;
@@ -107,6 +115,10 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 			continue;
 		}
 		else if (paths_amount == 4) {
+			// TODO: implement alpha and remove these
+			std::cerr << "[error] alpha support is under development, we don't handle it well yet 8)\n";
+			exit(1);
+			//
 			settings.has_alpha = true;
 			settings.a_input_path = current;
 			settings.a_input_ext = get_extension(current);
@@ -116,9 +128,14 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 			continue;
 		}
 		else {
-			cerr << "[error] more than 4 files specified, our images have max of only 4 color channels!\n";
+			cerr << "[error] more than 4 files specified, our images have a max of only 4 color channels!\n";
 			exit(1);
 		}
+	}
+
+	if (non_skipped_paths_amount == 0) {
+		std::cerr << "[error] at least one image path is needed to mix\n";
+		exit(1);
 	}
 
 	if (!is_a_valid_extension(settings.output_extension)) {
@@ -128,7 +145,7 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 
 	settings.output_extension = change_to_jpg_if_jpeg(settings.output_extension);
 
-// #ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
 	cout << "\napp settings: \n";
 	cout << "r input path "  << (settings.r_input_path == "" ? "NULL" : ("\""+settings.r_input_path+"\"")) << " with ext: " << settings.r_input_ext << "\n";
 	cout << "g input path "  << (settings.g_input_path == "" ? "NULL" : ("\""+settings.g_input_path+"\"")) << " with ext: " << settings.g_input_ext << "\n";
@@ -137,12 +154,174 @@ MixSettings create_settings_from_args(int argc, char* argv[])
 	cout << "a input path "  << (settings.a_input_path == "" ? "NULL" : ("\""+settings.a_input_path+"\"")) << " with ext: " << settings.a_input_ext << "\n";
 	cout << "out path "  << settings.output_path << "\n";
 	cout << "out ext "  << settings.output_extension << "\n";
-// #endif
+#endif
 
 	return settings;
 }
 
+int get_max_width_of(ImageData* r_ptr, ImageData* g_ptr, ImageData* b_ptr, ImageData* a_ptr)
+{
+	int max_width = 0;
+	
+	int current_img_width = 0;
+	if (r_ptr) {
+		current_img_width = r_ptr->get_width();
+		if (current_img_width > max_width) {
+			max_width = current_img_width;
+		}
+	}
+	if (g_ptr) {
+		current_img_width = g_ptr->get_width();
+		if (current_img_width > max_width) {
+			max_width = current_img_width;
+		}
+	}
+	if (b_ptr) {
+		current_img_width = b_ptr->get_width();
+		if (current_img_width > max_width) {
+			max_width = current_img_width;
+		}
+	}
+	if (a_ptr) {
+		current_img_width = a_ptr->get_width();
+		if (current_img_width > max_width) {
+			max_width = current_img_width;
+		}
+	}
+
+	return max_width;
+}
+
+int get_max_height_of(ImageData* r_ptr, ImageData* g_ptr, ImageData* b_ptr, ImageData* a_ptr)
+{
+	int max_height = 0;
+	
+	int current_img_height = 0;
+	if (r_ptr) {
+		current_img_height = r_ptr->get_height();
+		if (current_img_height > max_height) {
+			max_height = current_img_height;
+		}
+	}
+	if (g_ptr) {
+		current_img_height = g_ptr->get_height();
+		if (current_img_height > max_height) {
+			max_height = current_img_height;
+		}
+	}
+	if (b_ptr) {
+		current_img_height = b_ptr->get_height();
+		if (current_img_height > max_height) {
+			max_height = current_img_height;
+		}
+	}
+	if (a_ptr) {
+		current_img_height = a_ptr->get_height();
+		if (current_img_height > max_height) {
+			max_height = current_img_height;
+		}
+	}
+
+	return max_height;
+}
+
+void aggregate_red(const ImageData &src, ImageData &dest)
+{
+	int src_width  = src.get_width();
+	int src_height = src.get_height();
+
+	int dest_width  = dest.get_width();
+
+	for (int y = 0; y < src_height; ++y) {
+		for (int x = 0; x < src_width; ++x) {
+			int src_index = y * src_width + x;
+			int dest_index = y * dest_width + x;
+			dest.colors[dest_index].r = src.colors[src_index].r;
+		}
+	}
+}
+void aggregate_green(const ImageData &src, ImageData &dest)
+{
+	int src_width  = src.get_width();
+	int src_height = src.get_height();
+
+	int dest_width  = dest.get_width();
+
+	for (int y = 0; y < src_height; ++y) {
+		for (int x = 0; x < src_width; ++x) {
+			int src_index = y * src_width + x;
+			int dest_index = y * dest_width + x;
+			dest.colors[dest_index].g = src.colors[src_index].g;
+		}
+	}
+}
+void aggregate_blue(const ImageData &src, ImageData &dest)
+{
+	int src_width  = src.get_width();
+	int src_height = src.get_height();
+
+	int dest_width  = dest.get_width();
+
+	for (int y = 0; y < src_height; ++y) {
+		for (int x = 0; x < src_width; ++x) {
+			int src_index = y * src_width + x;
+			int dest_index = y * dest_width + x;
+			dest.colors[dest_index].b = src.colors[src_index].b;
+		}
+	}
+}
+
+// TODO: aggregate alpha
+
 int main(int argc, char *argv[])
 {
 	MixSettings settings = create_settings_from_args(argc, argv);
+
+	std::unique_ptr<ImageData> red_image_data_ptr = nullptr;
+	std::unique_ptr<ImageData> green_image_data_ptr = nullptr;
+	std::unique_ptr<ImageData> blue_image_data_ptr = nullptr;
+	
+	std::unique_ptr<ImageData> alpha_image_data_ptr = nullptr;
+	
+	if (!settings.r_input_path.empty()) {
+		read_function read_red_image = get_read_function(settings.r_input_ext);
+		red_image_data_ptr = read_red_image(settings.r_input_path.data());
+		exit_if_nullptr(red_image_data_ptr);
+	}
+
+	if (!settings.g_input_path.empty()) {
+		read_function read_green_image = get_read_function(settings.g_input_ext);
+		green_image_data_ptr = read_green_image(settings.g_input_path.data());
+		exit_if_nullptr(green_image_data_ptr);
+	}
+
+	if (!settings.b_input_path.empty()) {
+		read_function read_blue_image = get_read_function(settings.b_input_ext);
+		blue_image_data_ptr = read_blue_image(settings.b_input_path.data());
+		exit_if_nullptr(blue_image_data_ptr);
+	}
+
+	// TODO: handle alpha
+	// if (!settings.a_input_path.empty()) {
+	// 	read_function read_alpha_image = get_read_function(settings.a_input_ext);
+	// 	alpha_image_data_ptr = read_alpha_image(settings.a_input_path.data());
+	// 	exit_if_nullptr(alpha_image_data_ptr);
+	// }
+
+	WriteSettings write_settings(settings.output_path, settings.has_alpha);
+	write_function write_image = get_write_function(settings.output_extension);
+
+	int max_width  = get_max_width_of(red_image_data_ptr.get(), green_image_data_ptr.get(), blue_image_data_ptr.get(), alpha_image_data_ptr.get());
+	int max_height = get_max_height_of(red_image_data_ptr.get(), green_image_data_ptr.get(), blue_image_data_ptr.get(), alpha_image_data_ptr.get());
+	
+	std::cout << "[mix] creating image with width " << max_width << " and height " << max_height << "\n";
+
+	auto mixed_image = std::make_unique<ImageData>(max_width, max_height);
+	
+	if (red_image_data_ptr) aggregate_red(*red_image_data_ptr, *mixed_image);
+	if (green_image_data_ptr) aggregate_green(*green_image_data_ptr, *mixed_image);
+	if (blue_image_data_ptr) aggregate_blue(*blue_image_data_ptr, *mixed_image);
+	// aggregate_alpha(*alpha_image_data_ptr, *mixed_image);
+	
+	write_image(write_settings, *mixed_image);
 }
